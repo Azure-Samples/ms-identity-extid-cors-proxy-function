20391fbe7784075c606e5e3b925896d65c6ad72d
James Casey <james.casey@microsoft.com>
2024-11-27 16:25:11 +0000

    Use Axios

--- src/functions/httpProxyFunction.js
+++ src/functions/httpProxyFunction.js
@@ -1,13 +1,74 @@
-const { app } = require('@azure/functions');
+const { app } = require("@azure/functions");
+const  axios = require('axios');
 
-app.http('httpget', {
-    methods: ['GET'],
-    authLevel: 'function',
-    handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
+const tenantSubdomain = process.env.TENANT_SUBDOMAIN || '';
 
-        const name = request.query.get('name') || await request.text() || 'world';
+async function MyProxyFunction(request, context) {
+    try {
+        const hostName = `${tenantSubdomain}.ciamlogin.com`;
+        const baseURL = `https://${hostName}/${tenantSubdomain}.onmicrosoft.com`;
+        const incomingPath = request.params.path || '';
 
-        return { body: `Hello, ${name}!` };
+        const options = {
+            url: `${baseURL}/${incomingPath}`,
+            method: request.method,
+            headers: {
+                ...request.headers,
+                host: hostName,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            timeout:0,
+            decompress:false,
+            maxContentLength: 100000,
+            maxBodyLength: 100000,
+            responseType: 'text',
+            validateStatus: function (status) {
+                return 1; // Never an error
+            }              
+        };
+
+        const requestData = await request.text();
+        options.body = requestData;
+
+
+        context.log("HTTP Call");
+        //context.log("  BaseURL: " + options.baseURL);
+        context.log("  Path: " + options.url);
+        context.log("  Method: " + options.method);
+        context.log("  Headers: " + JSON.stringify(options.headers));
+        context.log("  Body: " + options.body);
+        
+        const response = await axios.request(options);
+        return {
+            status: response.status,
+            body: response.data,
+            headers: {
+                ...response.headers
+            }
+        };
+    } catch (error) {
+        return {
+            status: 500,
+            body: `Error: ${error.message || error}`
+        };
     }
-});
+};
+
+config = {
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    route: "{*path}",
+    authLevel: 'anonymous'
+}
+
+if (tenantSubdomain) {
+    config.handler = MyProxyFunction
+} else {
+    config.handler = async (req, context) => {
+        return {
+            status: 400,
+            body: "Tenant subdomain is not set"
+        }
+    }
+}
+
+app.http('authProxy', config);
